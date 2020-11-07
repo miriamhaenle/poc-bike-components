4e41aad0cbe73e129ac1da850801250ea3928235
Miriam <miriam.haenle@gmail.com>
2020-11-07 18:48:20 +0100

    Add test for id service

--- src/services/idService.js
+++ src/services/idService.js
@@ -1,5 +1,11 @@
+const idCache = new Set()
+
 export function createID() {
   const result = generateId().next()
+  if (idCache.has(result.value)) {
+    return createID()
+  }
+  idCache.add(result.value)
   return result.value
 }
 
---
+++ src/services/idService.test.js
@@ -0,0 +1,17 @@
+import { createID } from './idService'
+
+describe('#createID', () => {
+  it('should return a number', () => {
+    const result1 = createID()
+    expect(typeof result1).toBe('number')
+  })
+
+  it('should return a unique number', () => {
+    const result1 = createID()
+    const result2 = createID()
+    expect(typeof result1).toBe('number')
+    expect(typeof result2).toBe('number')
+    console.log(result1, result2)
+    expect(result1).not.toBe(result2)
+  })
+})
